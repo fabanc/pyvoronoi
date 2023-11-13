1395e44c466a1a510a1056aa5f72ab537ff44f68
fabanc <fabien.ancelin@gmail.com>
2023-11-13 16:18:28 -0500

    Updated VS C++ instructions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 from setuptools.extension import Extension
 from setuptools.command.test import test as TestCommand
 
-version = '1.0.8'
+version = '1.0.8.1'
 
 """
 Note on using the setup.py:
@@ -85,6 +85,14 @@ class build_ext_subclass( build_ext ):
                 userdir = os.environ["USERPROFILE"]
                 os.environ["INCLUDE"] = userdir + "\\AppData\\Local\\Programs\\Common\\Microsoft\\Visual C++ for Python\\9.0\\VC\\include\\"
 
+        # Starting from 3.11, the file longintrepr.h has moved. It is no longer under Python@3.XX\include but Python@3.XX\include\cpython        
+        elif sys.version_info.major == 3 and sys.version_info.minor >= 11:
+            c = self.compiler.compiler_type
+            if c == 'msvc':
+                for e in self.extensions:
+                    install_dir = os.path.dirname(sys.executable)
+                    cpython_directory = os.path.join(install_dir, 'include', 'cpython')         
+                    e.extra_compile_args = [cpython_directory]
         build_ext.build_extensions(self)
 
 setup(
