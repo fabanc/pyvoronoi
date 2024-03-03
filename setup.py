b551de4e0cb917eb36bb9ad6603a87a3ec6a2225
Fabien Ancelin <fancelin@esri.ca>
2024-03-03 12:41:15 -0500

    Package fix. Missing cpp files

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.extension import Extension
 from setuptools.command.test import test as TestCommand
 from pathlib import Path
 
-version = '1.0.242'
+version = '1.0.25'
 
 """
 Note on using the setup.py:
