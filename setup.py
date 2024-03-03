3581fd4d4d42fe039d32018114a04b3905fff6d3
Fabien Ancelin <fancelin@esri.ca>
2024-03-03 10:53:45 -0500

    Version bump prior to release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.extension import Extension
 from setuptools.command.test import test as TestCommand
 from pathlib import Path
 
-version = '1.0.22'
+version = '1.0.23'
 
 """
 Note on using the setup.py:
