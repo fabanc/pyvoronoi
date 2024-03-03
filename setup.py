7c354c4da96e2c9f09523cda7b57c3e4e2985f5e
Fabien Ancelin <fancelin@esri.ca>
2024-03-03 10:31:07 -0500

    Modifying workflows so pypi deployment is only done on main

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools.extension import Extension
 from setuptools.command.test import test as TestCommand
 from pathlib import Path
 
-version = '1.0.21'
+version = '1.0.22'
 
 """
 Note on using the setup.py:
