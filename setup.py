99c043773f598f34b1934397fec18fb36272b6a9
Fabien Ancelin <fancelin@esri.ca>
2024-04-03 21:22:26 -0400

    Version increase.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools.extension import Extension
 from setuptools.command.test import test as TestCommand
 from pathlib import Path
 
-version = '1.0.3'
+version = '1.0.4'
 
 """
 Note on using the setup.py:
